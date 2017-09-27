5fcdcc6a498dd3216d186c00733717b8165e66dd
BillChandler <bchandler.cca0@gmail.com>
2017-09-27 06:25:29 -0400

    Add clause and consclusion

--- sentance.js
+++ sentance.js
@@ -4,10 +4,22 @@ var btn1 = document.getElementById("b1");
 var btn2 = document.getElementById("b2");
 
 var intro = [
-    "Once upon a time",
-    "And so it was",
-    "In the beginning"
+    "Once upon a time, ",
+    "And so it was, ",
+    "In the beginning, "
 ];
+var clause = [
+
+    "was walking in the park, ",
+    "was wandering in the mall, ",
+    "was riding the Ferris Wheel, "
+];
+var conclusion = [
+    "when a bird landed on his head.",
+    "as Birnam wood came to Dunsinane.",
+    "when the sun went down in the East."
+]
+
 
 
 function getRandomInt(min, max){
@@ -24,15 +36,23 @@ function getRandomInt(min, max){
 
 btn1.addEventListener("click", function(){
     var message = "";
-    var i = getRandomInt(0,3);    console.log("bt1 has been clicked");
-    message += intro[i]+ " " + userName.value;
-    console.log(message);
-    resultArea.innerText = message;
+    var i = 0;
+    i = getRandomInt(0,3);    
+    message += intro[i]+ " " + userName.value + " ";
+    
+    i = i = 0;
+    i = getRandomInt(0,3); 
+    message += clause[i] + " ";
+
+    i = getRandomInt(0,3);
+    message += conclusion[i] + " ";
+
+    resultArea.innerHTML = "<ul><li>" + message + "</li></ul>";
 
 });
 
 btn2.addEventListener("click", function(){
     
-        console.log("bt2 has been clicked");
+        resultArea.innerHTML = "Gibberish soon to appear here.";
     
 });
